59ae60031af6df219b9bb93f636fb43ab0631735
mark988 <80692072@qq.com>
2019-04-24 02:36:29 +0800

    Add files via upload
    init

---
+++ testsessionA/src/main/java/com/example/demo/DemoAppA.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppA {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppA.class, args);
+	}
+
+}
