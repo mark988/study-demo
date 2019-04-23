435598568d7054853a41c1f7babef5bc52bd5932
mark988 <80692072@qq.com>
2019-04-24 02:39:43 +0800

    Add files via upload
    init

---
+++ testsessionB/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by 马晓光 on 2019/4/24.
+ */
+
+@Slf4j
+@RestController
+public class TestController  {
+    @RequestMapping("/set-session")
+    public Object setSession(String sessionValue,HttpSession httpSession){
+        httpSession.setAttribute("sessionVal",sessionValue);
+        return sessionValue;
+    }
+
+    @RequestMapping("/get-session")
+    public Object getSession(HttpSession httpSession){
+        Object o = httpSession.getAttribute("sessionVal");
+        log.info("B server");
+        return o;
+    }
+}
