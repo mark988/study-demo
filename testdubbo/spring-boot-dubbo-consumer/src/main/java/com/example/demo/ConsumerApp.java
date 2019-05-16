4960d28b7a93bcd2069c45e534ae8f0a87af9410
mark <maxiaoguang@encdata.cn>
2019-05-16 16:07:52 +0800

    删除多余的引入

--- testdubbo/spring-boot-dubbo-consumer/src/main/java/com/example/demo/ConsumerApp.java
+++ testdubbo/spring-boot-dubbo-consumer/src/main/java/com/example/demo/ConsumerApp.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
