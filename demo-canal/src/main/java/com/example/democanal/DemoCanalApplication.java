dfbfa685931eea75364f38b852528f8f47069607
mark <80692072@qq.com>
2019-06-26 18:21:38 +0800

    canal 测试

---
+++ demo-canal/src/main/java/com/example/democanal/DemoCanalApplication.java
@@ -0,0 +1,13 @@
+package com.example.democanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCanalApplication.class, args);
+    }
+
+}
