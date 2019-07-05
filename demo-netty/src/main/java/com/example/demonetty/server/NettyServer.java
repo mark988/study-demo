815430fad965b07bf051c55914403644a0f735ae
mark <80692072@qq.com>
2019-07-05 14:59:24 +0800

    netty server client  最简单启动流程init

---
+++ demo-netty/src/main/java/com/example/demonetty/server/NettyServer.java
@@ -0,0 +1,36 @@
+package com.example.demonetty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.springframework.stereotype.Component;
+
+/***
+ * @author : 马晓光
+ * @date   : 2019/7/5
+ * @description :
+ **/
+
+@Component
+public class NettyServer {
+
+    public  void start(){
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workGroup)
+                       .channel(NioServerSocketChannel.class)
+                       .childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+                           @Override
+                           protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+
+                           }
+                       });
+        serverBootstrap.bind(8888);
+    }
+
+}
