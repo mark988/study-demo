1a4a5813ca4d1509580fecde58d01fc3e9a5ecce
mark <80692072@qq.com>
2019-08-23 18:10:34 +0800

    使用jetty例子

---
+++ demo-jetty/src/main/java/com/example/demojetty/DemoJettyApp.java
@@ -0,0 +1,13 @@
+package com.example.demojetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJettyApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJettyApp.class, args);
+    }
+
+}
--- demo-jetty/src/main/java/com/example/demojetty/TestController.java
+++ demo-jetty/src/main/java/com/example/demojetty/TestController.java
@@ -1,9 +1,19 @@
 package com.example.demojetty;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /***
  * @author : 马晓光
  * @date   : 2019/8/23
  * @description :
  **/
+@RestController
 public class TestController {
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String test(){
+        return  "Hello world";
+    }
 }
