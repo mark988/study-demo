71fe61598998de1f2d45452931dd38599030d55a
mark <80692072@qq.com>
2020-04-22 10:38:06 +0800

    删除多余的代码

--- 02_根据不同用户切换数据源/src/main/java/com/example/demo/controller/TestController.java
+++ 02_根据不同用户切换数据源/src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.aop.EagleEye;
-import com.example.demo.entity.User;
-import com.example.demo.service.UserSlaveService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,32 +13,15 @@ import java.util.List;
 public class TestController {
 
     @Autowired
-    private UserSlaveService userService;
-
-    @Autowired
-    private UserService userService2;
-
-
-    /**
-     * 方法上使用了注解DS ，所以DS注解为准
-     * @return
-     */
-    @RequestMapping(value = "/slave",method = RequestMethod.GET)
-    public  String  test(){
-        User u = new User();
-        u.setName("Tom online");
-        u.setAge((int) (Math.random()*(30)));
-        userService.addUser(u);
-        return "操作slave数据库";
-    }
+    private UserService userService;
 
     /**
      * 方法上没有使用DS注解，所以判断过来的方式是否带有list等，查询直接走slave
      * @return
      */
     @EagleEye
-    @RequestMapping(value = "/slave2",method = RequestMethod.GET)
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
     public List test2(){
-        return userService2.listUser();
+        return userService.listUser();
     }
 }
--- 02_根据不同用户切换数据源/src/main/java/com/example/demo/service/UserSlaveService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.demo.entity.User;
-public interface UserSlaveService extends IService<User> {
-
-  void addUser(User user);
-}
